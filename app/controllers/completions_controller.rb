40df807a1b8433290c4308e5f04ea94fcd7e989c
Ben Ricker <BGRicker@gmail.com>
2015-10-18 14:07:45 -0400

    refactor complete/completed at

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,7 +1,7 @@
 class CompletionsController < ApplicationController
 
   def create
-    current_user.todos.find(params[:todo_id]).touch :completed_at
+    todo.complete!
     redirect_to todos_path
   end
 
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -6,4 +6,21 @@ describe Todo, "#completed?" do
 
     expect(todo).to be_completed
   end
+    it "returns false if completed as is nil" do
+      todo = Todo.new(completed_at: nil)
+  
+      expect(todo).not_to be_completed
+    end
+end
+
+describe Todo, "#complete!" do
+  it "update completed at" do
+    todo = Todo.create!(completed_at: nil)
+
+    todo.complete!
+
+    todo.reload
+
+    expect(todo).to be_completed
+  end
 end

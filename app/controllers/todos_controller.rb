0f0d7d6df8c1ffe24b9797c705e31d2936d2d991
Ben <ben@Bens-iMac.lan>
2015-10-17 21:56:16 -0400

    user model, refactored controllers

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,8 @@
 class TodosController < ApplicationController
   before_filter :authenticate
+
   def index
-    @todos = Todo.where(email: session[:current_email])
+    @todos = current_user.todos
   end
 
   def new
@@ -9,10 +10,10 @@ class TodosController < ApplicationController
   end
 
   def create
-    Todo.create(todo_params.merge(email: current_email))
+    current_user.todos.create(todo_params)
     redirect_to todos_path
   end
-  
+
   private
 
   def todo_params
